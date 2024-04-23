3d141a128e6f1bfa93963192c7bd62b029a62082
minhyeok Bang <bmh89895656@gmail.com>
2024-04-23 14:48:19 +0900

    bmh

--- back/src/main/java/com/shop/back/item/repository/ItemRepository.java
+++ back/src/main/java/com/shop/back/item/repository/ItemRepository.java
@@ -15,18 +15,18 @@ public interface ItemRepository extends JpaRepository<Item, Long> ,QuerydslPredi
 
 	List<Item> findByName(String name);
 
-	List<Item> findByNameOrContent(String name, String content);
+//	List<Item> findByNameOrContent(String name, String content);
 	
 	// 상품가격 내림차순 정렬
-	List<Item> findByPriceLessThanOrderByPriceDesc(int price);
+//	List<Item> findByPriceLessThanOrderByPriceDesc(int price);
 	
 	// JPQL [관점 : Entity]
-	@Query("select i from Item i where i.content like %:content% order by i.price desc")
-	List<Item> findbyItemContent (@Param("content") String content);
+//	@Query("select i from Item i where i.content like %:content% order by i.price desc")
+//	List<Item> findbyItemContent (@Param("content") String content);
 	
 	//Native [관점 : 테이블]
-	@Query(value = "select * from Item i where i.content like %:content% order by i.price asc" , nativeQuery = true)
-	List<Item> findbyItemContentNative (@Param("content") String content);
+//	@Query(value = "select * from Item i where i.content like %:content% order by i.price asc" , nativeQuery = true)
+//	List<Item> findbyItemContentNative (@Param("content") String content);
 	
 	
 }
